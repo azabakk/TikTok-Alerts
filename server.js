4f3ec3494b38e9a1362d030d4c3994cd2cc98332
azabakk <120814498+azabakk@users.noreply.github.com>
2026-08-24 21:59:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const { WebcastPushConnection } = require('tiktok-live-connector');
+const TikTokLive = require('tiktok-live-connector');
+const WebcastPushConnection = TikTokLive.WebcastPushConnection || TikTokLive;
+
 
 const app = express();
 const server = http.createServer(app);
